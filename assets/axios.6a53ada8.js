1f64767ab5dee1d1a00fcd7399de3f31dbfe43af
dppb2012@gmail.com <dppb2012@gmail.com>
2022-06-10 18:59:20 +0900

    first

---
+++ assets/axios.6a53ada8.js
@@ -0,0 +1,1 @@
+import{h as i}from"./vendor.863e1bd7.js";import{a}from"./index.0cb4c3ba.js";const e=a.create({baseURL:"https://api.example.com"});var t=i(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=e});export{e as api,t as default};
---
+++ assets/use-dark.8add7e15.js
@@ -0,0 +1,1 @@
+import{i as e}from"./vendor.863e1bd7.js";const u={dark:{type:Boolean,default:null}};function o(a,r){return e(()=>a.dark===null?r.dark.isActive:a.dark)}export{o as a,u};
